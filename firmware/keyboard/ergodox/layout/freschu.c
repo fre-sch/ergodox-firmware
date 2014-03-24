05796042138a085d602e52fe7f8745f05c1f4c78
Frederik Schumacher <frederik.schumacher@gmail.com>
2014-03-24 07:23:33 +0100

    defines for more ease in writing german layout. trying some layer2 helpers for my dvorak software layout

--- firmware/keyboard/ergodox/layout/freschu.c
+++ firmware/keyboard/ergodox/layout/freschu.c
@@ -12,25 +12,34 @@
 #include "./fragments/matrix-control.part.h"
 
 
+#define de_y    z
+#define de_z    y
+#define de_oe   semicol
+#define de_ae   quote
+#define de_ue   brktL
+#define sharp   nonUSPound
+#define de_grave nonUSBkslash
+
+
 static layout_t layout PROGMEM = {
     MATRIX_LAYER(  // layer 0 : default
 // macro, unused,
        K,    nop,
 // left hand ...... ......... ......... ......... ......... ......... .........
-     nop,        1,        2,        3,        4,        5,nonUSBkslash,
+     nop,        1,        2,        3,        4,        5, de_grave,
      tab,        q,        w,        e,        r,        t,      esc,
    ctrlL,        a,        s,        d,        f,        g,
-   grave,    z,        x,        c,        v,        b,      altL,
+   grave,     de_y,        x,        c,        v,        b,      altL,
      nop,      nop,      nop,      nop,      nop,
                                                                  nop, lpupo3l3,
                                                        nop,      nop,      nop,
                                                     shiftL,     guiL, lpupo1l1,
 // right hand ..... ......... ......... ......... ......... ......... .........
               dash,        6,        7,        8,        9,        0,      nop,
-             enter,        y,        u,        i,        o,        p,    brktL,
-                           h,        j,        k,        l,  semicol,    quote,
+             enter,     de_z,        u,        i,        o,        p,    de_ue,
+                           h,        j,        k,        l,    de_oe,    de_ae,
                 bs,        n,        m,    comma,   period,    slash,    brktR,
-                                  altR,      nop,      nop,nonUSPound,    ctrlR,
+                                  altR,      nop,      nop,    sharp,    ctrlR,
      nop,      nop,
      nop,      nop,      nop,
 lpupo2l2,   shiftR,    space  ),
@@ -38,30 +47,6 @@ lpupo2l2,   shiftR,    space  ),
 // ............................................................................
 
     MATRIX_LAYER(  // layer 1 : number pad
-// macro, unused,
-       K,    nop,
-// left hand ...... ......... ......... ......... ......... ......... .........
-  transp,   transp,   transp,   transp,   transp,   transp,   transp,
-  transp,   transp,   transp,   transp,   transp,   transp,   transp,
-  transp,   transp,   transp,   transp,   transp,   transp,
-  transp,   transp,   transp,   transp,   transp,   transp,   transp,
-  transp,   transp,   transp,   transp,   transp,
-                                                              transp,   transp,
-                                                    transp,   transp,   transp,
-                                                    transp,   transp,   transp,
-// right hand ..... ......... ......... ......... ......... ......... .........
-            transp,   transp,   transp,   transp,   transp,   transp,   transp,
-            transp,     home,    pageD,   arrowU,    pageU,   transp,   transp,
-                         end,   arrowL,   arrowD,   arrowR,   transp,   transp,
-            transp,   transp,   transp,   transp,   transp,   transp,   transp,
-                                transp,   transp,   transp,   transp,   transp,
-  transp,   transp,
-  transp,   transp,   transp,
-  transp,   transp,   transp  ),
-
-// ............................................................................
-
-    MATRIX_LAYER(  // layer 2 : symbols and function keys
 // macro, unused,
        K,    nop,
 // left hand ...... ......... ......... ......... ......... ......... .........
@@ -74,7 +59,31 @@ lpupo2l2,   shiftR,    space  ),
                                                     transp,   transp,   transp,
                                                     transp,   transp,   transp,
 // right hand ..... ......... ......... ......... ......... ......... .........
-               F12,       F6,       F7,       F8,       F9,      F10,    power,
+               F12,       F6,       F7,       F8,       F9,      F10,   transp,
+            transp,     home,    pageD,   arrowU,    pageU,   transp,   transp,
+                         end,   arrowL,   arrowD,   arrowR,   transp,   transp,
+               del,   transp,   transp,   transp,   transp,   transp,   transp,
+                                transp,   transp,   transp,   transp,   transp,
+  transp,   transp,
+  transp,   transp,   transp,
+  transp,   transp,   transp  ),
+
+// ............................................................................
+
+    MATRIX_LAYER(  // layer 2 : symbols and function keys
+// macro, unused,
+       K,    nop,
+// left hand ...... ......... ......... ......... ......... ......... .........
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,    grave,        s,   transp,   transp,        q,
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,
+                                                              transp,   transp,
+                                                    transp,   transp,   transp,
+                                                    transp,   transp,   transp,
+// right hand ..... ......... ......... ......... ......... ......... .........
+            transp,   transp,   transp,   transp,   transp,   transp,    power,
             transp,   transp,   transp,   transp,   transp,   transp,  volumeU,
                       transp,   transp,   transp,   transp,   transp,  volumeD,
             transp,   transp,   transp,   transp,   transp,   transp,     mute,
