91a146b370054fd37a8745d64f1e4ec9724066c1
Frederik Schumacher <frederik.schumacher@gmail.com>
2014-04-15 22:54:26 +0200

    layout update, this time based on ISO-US

--- firmware/keyboard/ergodox/layout/freschu.c
+++ firmware/keyboard/ergodox/layout/freschu.c
@@ -12,60 +12,70 @@
 #include "./fragments/matrix-control.part.h"
 
 
-#define de_y      z
-#define de_z      y
-#define de_oe     semicol
-#define de_ae     quote
-#define de_ue     brktL
-#define de_sharp  nonUSPound
-#define de_circum nonUSBkslash
-#define de_sz     dash
-#define de_grave  equal
-#define de_less   grave
-#define de_dash   slash
-#define de_plus   brktR
-
-
 static layout_t layout PROGMEM = {
     MATRIX_LAYER(  // layer 0 : default
 // macro, unused,
        K,    nop,
 // left hand ...... ......... ......... ......... ......... ......... .........
-     nop,        1,        2,        3,        4,        5,de_circum,
-     esc,        q,        w,        e,        r,        t,      tab,
-   ctrlL,        a,        s,        d,        f,        g,
- de_less,     de_y,        x,        c,        v,        b,      altL,
-     nop,      nop,      nop,      nop,      nop,
-                                                                altL, lpupo3l3,
+     nop,        1,        2,        3,        4,        5,      nop,
+     esc,  semicol,    comma,   period,        p,        y,      esc,
+   ctrlL,        a,        o,        e,        i,        u,
+lpupo1l1,    slash,        q,        j,        k,        x,      tab,
+    altL,      nop,      nop,      nop, lpupo2l2,
+                                                                 nop,      nop,
                                                        nop,      nop,      nop,
-                                                    shiftL,     guiL, lpupo1l1,
+                                                    shiftL,     guiL,     altL,
 // right hand ..... ......... ......... ......... ......... ......... .........
-          de_grave,        6,        7,        8,        9,        0,    de_sz,
-             enter,     de_z,        u,        i,        o,        p,    de_ue,
-                           h,        j,        k,        l,    de_oe,    de_ae,
-                bs,        n,        m,    comma,   period,  de_dash, de_sharp,
-                                  altR,    ctrlR,      nop,      nop,  de_plus,
-     nop,    ctrlR,
+               nop,        6,        7,        8,        9,        0,      nop,
+             enter,        f,        g,        c,        t,        z,      nop,
+                           h,        d,        r,        n,        s,    ctrlR,
+                bs,        b,        m,        w,        v,        l, lpupo1l1,
+                              lpupo2l2,      nop,      nop,      nop,     altR,
+lpupo3l3,      nop,
      nop,      nop,      nop,
-lpupo2l2,   shiftR,    space  ),
+    altR,   shiftR,    space  ),
 
-// ............................................................................
+// ========================================================================= //
 
     MATRIX_LAYER(  // layer 1
 // macro, unused,
        K,    nop,
 // left hand ...... ......... ......... ......... ......... ......... .........
+  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,    brktL,    brktR,   transp,
+  transp,       at,   transp,   transp,   parenL,   parenR,
+     nop,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,   transp,   transp,   transp,   transp,
+                                                              transp,   transp,
+                                                    transp,   transp,   transp,
+                                                    transp,   transp,   transp,
+// right hand ..... ......... ......... ......... ......... ......... .........
+            transp,   transp,   transp,   transp,   transp,   transp,   transp,
+            transp,   transp,    quote,    grave,   transp,   transp,   transp,
+                      transp,     dash,    equal,  bkslash,   transp,   transp,
+            transp,   transp,   transp,   transp,   transp,   transp,      nop,
+                                transp,   transp,   transp,   transp,   transp,
+  transp,   transp,
+  transp,   transp,   transp,
+  transp,   transp,   undersc  ),
+
+// ========================================================================= //
+
+    MATRIX_LAYER(  // layer 2
+// macro, unused,
+       K,    nop,
+// left hand ...... ......... ......... ......... ......... ......... .........
   transp,       F1,       F2,       F3,       F4,       F5,      F11,
-  transp,   transp,   transp,   transp,   transp,   transp,   transp,
-  transp,   transp,   transp,   transp,   transp,   transp,
-  transp,   transp,   transp,   transp,   transp,   transp,   transp,
+  transp,        1,        2,        3,        4,        5,   transp,
+  transp,        6,        7,        8,        9,        0,
+     nop,   transp,   transp,   transp,   transp,   transp,   transp,
   transp,   transp,   transp,   transp,   transp,
                                                               transp,   transp,
                                                     transp,   transp,   transp,
                                                     transp,   transp,   transp,
 // right hand ..... ......... ......... ......... ......... ......... .........
                F12,       F6,       F7,       F8,       F9,      F10,    power,
-            transp,     home,    pageD,   arrowU,    pageU,   transp,  volumeU,
+           kpEnter,     home,    pageD,   arrowU,    pageU,   transp,  volumeU,
                          end,   arrowL,   arrowD,   arrowR,   transp,  volumeD,
                del,   transp,   transp,   transp,   transp,   transp,     mute,
                                 transp,   transp,   transp,   transp,   transp,
@@ -73,37 +83,13 @@ lpupo2l2,   shiftR,    space  ),
   transp,   transp,   transp,
   transp,   transp,   transp  ),
 
-// ............................................................................
-
-    MATRIX_LAYER(  // layer 2
-// macro, unused,
-       K,    nop,
-// left hand ...... ......... ......... ......... ......... ......... .........
-  transp,   transp,   transp,   transp,   transp,   transp,   transp,
-  transp,  de_plus,   transp,   transp,   transp,   transp,   transp,
-  transp,  de_less,     de_y,   transp,   transp,        q,
-  transp,   transp,   transp,   transp,   transp,   transp,   transp,
-  transp,   transp,   transp,   transp,   transp,
-                                                              transp,   transp,
-                                                    transp,   transp,   transp,
-                                                    transp,   transp,   transp,
-// right hand ..... ......... ......... ......... ......... ......... .........
-            transp,   transp,   transp,   transp,   transp,   transp,   transp,
-            transp,   transp,   transp,   transp,   transp,   transp,   transp,
-                      transp,   transp,   transp,   transp,   transp,   transp,
-            transp,   transp,   transp,   transp,   transp,   transp,   transp,
-                                transp,   transp,   transp,   transp,   transp,
-  transp,   transp,
-  transp,   transp,   transp,
-  transp,   transp,   transp  ),
-
-// ............................................................................
+// ========================================================================= //
 
     MATRIX_LAYER(  // layer 3 : keyboard functions
 // macro, unused,
        K,    nop,
 // left hand ...... ......... ......... ......... ......... ......... .........
-   btldr,      nop,      nop,      nop,      nop,      nop,      nop,
+     nop,      nop,      nop,      nop,      nop,      nop,      nop,
      nop,      nop,      nop,      nop,      nop,      nop,      nop,
      nop,      nop,      nop,      nop,      nop,      nop,
      nop,      nop,      nop,      nop,      nop,      nop,      nop,
@@ -112,10 +98,10 @@ lpupo2l2,   shiftR,    space  ),
                                                        nop,      nop,      nop,
                                                        nop,      nop,      nop,
 // right hand ..... ......... ......... ......... ......... ......... .........
+               nop,      nop,      nop,      nop,      nop,      nop,    btldr,
                nop,      nop,      nop,      nop,      nop,      nop, dmp_sram,
-               nop,      nop,      nop,      nop,      nop,      nop, dmp_prog,
-                         nop,      nop,      nop,      nop,      nop, dmp_eepr,
-               nop,      nop,      nop,      nop,      nop,      nop,      nop,
+                         nop,      nop,      nop,      nop,      nop, dmp_prog,
+               nop,      nop,      nop,      nop,      nop,      nop, dmp_eepr,
                                    nop,      nop,      nop,      nop,      nop,
      nop,      nop,
      nop,      nop,      nop,
